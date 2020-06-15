0fb299b0322c404c18d57fb4697ede215d1b57ac
그라운드83 <ground83@geulaundeu83ui-iMac.local>
2020-06-16 05:19:56 +0900

    model.user

--- users/models.py
+++ users/models.py
@@ -11,15 +11,6 @@ class User(AbstractUser):
 
         verbose_name_plural = "고객"
 
-    GENDER_MALE = "male"
-    GENDER_FAMALE = " female"
-    GENDER_CHOICES = (
-        (GENDER_MALE, "남성"),
-        (GENDER_FAMALE, "여성"),
-    )
-    gender = models.CharField(
-        choices=GENDER_CHOICES, max_length=10, null=True, verbose_name="성별"
-    )
     # destination = models.ManyToManyField(
     #     "destinations.Destination",
     #     related_name="desitni",
@@ -27,11 +18,16 @@ class User(AbstractUser):
     #     verbose_name="도착지",
     #     null=False,
     # )
-    email = models.CharField(max_length=30, null=False, unique=True, verbose_name="이메일")
-    phone = models.CharField(max_length=11, null=True, unique=True, verbose_name="전화번호")
-    birthdate = models.DateField(null=True, verbose_name="생년월일")
-    policy = models.BooleanField(default=False, verbose_name="개인정보처리방침")
-    servicepolicy = models.BooleanField(default=False, verbose_name="서비스이용약관")
+    email = models.CharField(max_length=40, null=False, unique=True, verbose_name="이메일")
+    phone = models.CharField(
+        max_length=11, null=True, unique=True, blank=True, verbose_name="전화번호"
+    )
+    policy = models.BooleanField(
+        default=False, blank=True, null=True, verbose_name="개인정보처리방침"
+    )
+    servicepolicy = models.BooleanField(
+        blank=True, default=False, null=True, verbose_name="서비스이용약관"
+    )
     objects = core_managers.CustomUserManager()
     # favorites = models.ManyToManyField(
     #     "favorites.Favorite", related_name="favorites", blank=True
